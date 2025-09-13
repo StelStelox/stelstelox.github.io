5700b68d71c8de6eb7694be35c07a750fdf85efb
StelStelox <stelstelox@gmail.com>
2025-09-13 14:06:31 +0300

    update app pages

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import "./style.css";
+import avatar from "/stel_stelox.png";
+import github from "/github-mark-white.svg"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
+    <img src="${avatar}" class="avatar" alt="Avatar"/>
     <h1>Stel Stelox</h1>
-    <p class="read-the-docs">
-      Тут пока что пусто...
-    </p>
+    <a href="https://github.com/StelStelox" target="_blank">
+      <img src="${github}" class="github_logo"/>
+    </a>
   </div>
 `;
